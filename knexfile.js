ff73dca455af6be9e4d7fc90168cfb375dbfff5c
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-16 13:44:51 -0700

    initial commit server setup

---
+++ knexfile.js
@@ -0,0 +1,41 @@
+// Update with your config settings.
+
+module.exports = {
+	development: {
+		client: 'sqlite3',
+		useNullAsDefault: true,
+		connection: {
+			filename: './data/users.db3'
+		},
+		migrations: {
+			directory: './data/migrations'
+		},
+		seeds: {
+			directory: './data/seeds'
+		},
+		pool: {
+			afterCreate: (connection, done) => {
+				connection.run('PRAGMA foreign_keys = ON', done);
+			}
+		}
+	},
+
+	testing: {
+		client: 'sqlite3',
+		useNullAsDefault: true,
+		connection: {
+			filename: './data/test.db3'
+		},
+		migrations: {
+			directory: './data/migrations'
+		},
+		seeds: {
+			directory: './data/seeds'
+		},
+		pool: {
+			afterCreate: (connection, done) => {
+				connection.run('PRAGMA foreign_keys = ON', done);
+			}
+		}
+	}
+};
